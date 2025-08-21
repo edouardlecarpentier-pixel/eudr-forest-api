598e5c8ebd074a970aaa67a27955ff9532ddd2b6
edouardlecarpentier-pixel <edouard.lecarpentier@gmail.com>
2025-08-21 16:20:23 +0200

    Ajout des routes / et /favicon.ico pour corriger les erreurs 404Ajout des routes / et /favicon.ico pour corriger les erreurs 404Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ def verifier_coordonnees_en_ligne(geojson):
     coordinates = geojson.get("geometry", {}).get("coordinates", None)
     if not coordinates or not isinstance(coordinates, list):
         return {"error": "Coordonnées GeoJSON invalides."}
-
     lon, lat = coordinates
     url_verif = f"https://api.openforest.ai/v1/eudr?lon={lon}&lat={lat}"
     try:
@@ -17,11 +16,19 @@ def verifier_coordonnees_en_ligne(geojson):
         resultat = {"status": "erreur", "message": "Service non accessible"}
     return resultat
 
+@app.route('/')
+def home():
+    return jsonify({"message": "Bienvenue sur l'API EUDR Forest ! Utilise POST /verifier."})
+
+@app.route('/favicon.ico')
+def favicon():
+    return '', 204
+
 @app.route('/verifier', methods=['POST'])
 def verifier():
     data = request.get_json()
     if not data or "geojson" not in data:
-        return jsonify({"error": "Requête invalide : champ 'geojson' absent."}), 400
+        return jsonify({"error": "Requête invalide : champ 'geojson' absent."}), 400
     geojson = data.get("geojson")
     res = verifier_coordonnees_en_ligne(geojson)
     return jsonify(res)
